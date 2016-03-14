3ec405e33ba4c6b76b35f648741e0585eeadcc00
Mihail-K <Mihail.K@fimu.tk>
2016-03-13 23:38:27 -0400

    Add package file.

---
+++ source/pattern/package.d
@@ -0,0 +1,11 @@
+
+module pattern;
+
+public import pattern.base;
+public import pattern.brackets;
+public import pattern.complements;
+public import pattern.optionals;
+public import pattern.primitives;
+public import pattern.repeats;
+public import pattern.selections;
+public import pattern.sequences;
